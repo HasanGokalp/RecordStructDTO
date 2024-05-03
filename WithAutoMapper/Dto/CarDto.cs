6657615b083a2f71fb8a151ae1086376e9443421
Hasan GÖKALP <Hasan GKALP@DESKTOP-R04PVQ3>
2024-05-03 10:47:38 +0300

    list add

--- WithAutoMapper/Dto/CarDto.cs
+++ WithAutoMapper/Dto/CarDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace WithAutoMapper.Dto
 {
-    public record struct CarDto(int id, string name)
+    public record struct CarDto(int id, string name, string ex, ICollection<string> list)
     {
     }
 }
--- WithAutoMapper/Program.cs
+++ WithAutoMapper/Program.cs
@@ -7,20 +7,20 @@ class Program
     static void Main(string[] args)
     {
         var config = new MapperConfiguration(cfg => {
-            cfg.CreateMap<Car, CarDto>().ReverseMap();
+            cfg.CreateMap<Car, CarDto>().ForCtorParam("list", x => x.MapFrom(x => x.List)).ReverseMap();
         });
         IMapper mapper = config.CreateMapper();
 
-        var myClass = new Car { Id = 1, Name = "haso" };
+        var myClass = new Car { Id = 1, Name = "haso", Description = "hello", List = new List<string> { "hasan","sefa"} };
 
         var dst = mapper.Map<Car, CarDto>(myClass);
 
-        var myClass2 = new CarDto { id = 1, name = "hasoo" };
+        var myClass2 = new CarDto { id = 1, name = "hasoo", list = new List<string>() {"deneme" } };
 
         var dst2 = mapper.Map<CarDto, Car>(myClass2);
 
         Console.WriteLine(dst.name);
-        Console.WriteLine(dst2.Name);
+        Console.WriteLine(dst2.Description);
 
     }
 }
